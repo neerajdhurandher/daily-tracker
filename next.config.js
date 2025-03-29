e50016e95e5d8f738f33ef2932c155047b8c9bd4
neerajdhurandher <neerajdhurandar@gmail.com>
2025-03-29 13:27:44 +0530

    update next.config.js file for export

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 module.exports = {
   reactStrictMode: true,
   output: 'export',
-  exportTrailingSlash: true,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' }
-    }
-  }
 };
